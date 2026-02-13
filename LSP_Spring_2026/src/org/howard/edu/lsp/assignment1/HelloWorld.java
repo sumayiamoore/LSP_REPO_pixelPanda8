84fd6a35d1e0de8b3bc32330d7c114c697daf722
Sumayia Moore <sadeya.moore@gmail.com>
2026-02-12 21:11:05 -0500

    This is my first commit.

---
+++ LSP_Spring_2026/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,14 @@
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+	
+	/**
+	 * Name: Sumayia Moore
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!!!");
+
+	}
+
+}
